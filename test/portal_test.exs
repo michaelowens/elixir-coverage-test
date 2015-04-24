9cb89edbf369857c064e3a9b017a75299ceb6f92
Michael Owens <michaelowensnl@gmail.com>
2015-04-24 15:54:00 +0200

    Add test for Inspect.Portal, not sure why 2 lines not tested

--- test/portal_test.exs
+++ test/portal_test.exs
@@ -1,21 +1,25 @@
 defmodule PortalTest do
   use ExUnit.Case
 
-  setup do
-    Portal.shoot(:orange)
-    Portal.shoot(:blue)
-    :ok
+  """
+  #Portal<
+         :orange <=> :blue
+    [1, 2, 3, 4] <=> []
+  >
+  """
+  test "Custom Portal inspector" do
+    Portal.Door.start_link(:red)
+    Portal.Door.start_link(:green)
+    portal = Portal.transfer(:red, :green, [1,2,3,4])
+    assert Regex.match?(~r/^#Portal<\n(.+?) <=> (.+?)\n(.+?) <=> (.+?)\n>$/m, inspect(portal))
   end
 
-  test "Portal exist" do
+  test "Can push data between portals" do
+    Portal.shoot(:orange)
+    Portal.shoot(:blue)
     portal = Portal.transfer(:orange, :blue, [1,2,3,4])
     Portal.push_right(portal)
     assert Portal.Door.get(:orange) == [3,2,1]
     assert Portal.Door.get(:blue) == [4]
-    assert 1 + 1 == 2
   end
-
-  # test "Portal exist again", context do
-  #   assert 1 + 3 == 2
-  # end
 end
